474c00cb93d6145adff72869dd0c01f548444a46
G-districts <admin@gdistrict.org>
2025-11-23 17:50:00 -0800

    Create render-build.sh

---
+++ bin/render-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Exit immediately on error
+set -o errexit
+
+echo "==> bundle install"
+bundle install
+
+echo "==> yarn install"
+yarn install --pure-lockfile
+
+echo "==> compile Canvas assets"
+bundle exec rails canvas:compile_assets
+
+echo "==> migrate database"
+bundle exec rails db:migrate
